1b953466aaa255bf2855f2dbb60f2c5ac82bef10
Deondre <deondre.garcia@yahoo.com>
2022-09-22 00:47:51 -0700

    crypto bot section finished

--- src/sections/BirdNest/BirdNest.tsx
+++ src/sections/BirdNest/BirdNest.tsx
@@ -7,87 +7,138 @@ const birdNestOptions = {
   threshold: 0.8,
 };
 
-const birdNestObserverLeft = new IntersectionObserver(function(entries, birdNestObserverLeft) {
-  entries.forEach(entry => {
+const birdNestObserverLeft = new IntersectionObserver(function (
+  entries,
+  birdNestObserverLeft
+) {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
       entry.target.classList.add("slide-in-left");
       birdNestObserverLeft.unobserve(entry.target);
     }
-  })
-}, birdNestOptions);
+  });
+},
+birdNestOptions);
 
-const birdNestObserverRight = new IntersectionObserver(function(entries, birdNestObserverRight) {
-  entries.forEach(entry => {
+const birdNestObserverRight = new IntersectionObserver(function (
+  entries,
+  birdNestObserverRight
+) {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
       entry.target.classList.add("slide-in-right");
       birdNestObserverRight.unobserve(entry.target);
     }
-  })
-}, birdNestOptions);
+  });
+},
+birdNestOptions);
 
 const BirdNest = () => {
-
   useEffect(() => {
     const birdNestFeaturesLeft = document.querySelectorAll(".bn-feature-left");
-    birdNestFeaturesLeft.forEach(feature => {
+    birdNestFeaturesLeft.forEach((feature) => {
       birdNestObserverLeft.observe(feature);
-    })
+    });
 
-    const birdNestFeaturesRight = document.querySelectorAll(".bn-feature-right");
-    birdNestFeaturesRight.forEach(feature => {
+    const birdNestFeaturesRight =
+      document.querySelectorAll(".bn-feature-right");
+    birdNestFeaturesRight.forEach((feature) => {
       birdNestObserverRight.observe(feature);
-    })
-  
+    });
+
     return () => {
       birdNestObserverLeft.disconnect();
       birdNestObserverRight.disconnect();
-    }
-  }, [])
-  
+    };
+  }, []);
 
-  return <div className="bn-container">
-    <p className="bn-header">Bird Nest</p>
-    <p className="bn-subheader">
-        Tech Stack: React Native, Javascript, Redux, Node, Express, and MySQL 
+  return (
+    <div className="bn-container">
+      <p className="bn-header">Bird Nest</p>
+      <p className="bn-subheader">
+        Tech Stack: React Native, Javascript, Redux, Node, Express, and MySQL
       </p>
-      <a className="bn-link" href="https://github.com/deondregarcia/bird-nest" target="_blank">
+      <a
+        className="bn-link"
+        href="https://github.com/deondregarcia/bird-nest"
+        target="_blank"
+      >
         <h2>Link to GitHub</h2>
       </a>
-    <div className="bn-separator" />
-    <div className="bn-what">
-      <h3 className="bn-what-header">What is Bird Nest?</h3>
-      <p className="bn-header-text">Bird Nest is a React Native mobile app which provides a faster and more intuitive method of finding housing and roommates for university students.</p>
-    </div>
-    <div className="bn-separator" />
+      <div className="bn-separator" />
+      <div className="bn-what">
+        <h3 className="bn-what-header">What is Bird Nest?</h3>
+        <p className="bn-header-text">
+          Bird Nest is a React Native mobile app I developed as{" "}
+          <strong>Product Owner</strong> in a team of 7 as part of my San Diego
+          Supercomputer Center. The app provides a faster and more intuitive
+          method of finding housing and roommates for university students.
+        </p>
+      </div>
+      <div className="bn-separator" />
       <h3 className="bn-features-header">Features</h3>
-    <div className="bn-features">
-      <div className="bn-features-col">
-        <p className="bn-features-text bn-feature-right">Personal profiles with <strong>questionnaires</strong> and displayed traits geared towards housing preferences</p>
-        <p className="bn-features-text bn-feature-left"><strong>Sorting algorithms</strong> that rank potential roommates based on number of matched preferences</p>
+      <div className="bn-features">
+        <div className="bn-features-col">
+          <p className="bn-features-text bn-feature-right">
+            Personal profiles with <strong>questionnaires</strong> and displayed
+            traits geared towards housing preferences
+          </p>
+          <p className="bn-features-text bn-feature-left">
+            <strong>Sorting algorithms</strong> that rank potential roommates
+            based on number of matched preferences
+          </p>
+        </div>
+        <div className="bn-features-col bn-col-right">
+          <p className="bn-features-text bn-feature-right">
+            <strong>Filtering algorithms</strong> which, on top of the sorting
+            feature, allows users to apply more granular and variable
+            specifications on which type of roommate is desired
+          </p>
+          <p className="bn-features-text bn-feature-left">
+            <strong>Messaging</strong>, profile editing,{" "}
+            <strong>matchmaking</strong>, image uploading, and much, much more
+          </p>
+        </div>
       </div>
-      <div className="bn-features-col bn-col-right">
-      <p className="bn-features-text bn-feature-right"><strong>Filtering algorithms</strong> which, on top of the sorting feature, allows users to apply more granular and variable specifications on which type of roommate is desired</p>
-      <p className="bn-features-text bn-feature-left"><strong>Messaging</strong>, profile editing, <strong>matchmaking</strong>, image uploading, and much, much more</p>
-      </div>
-    </div>
-    <div className="bn-separator" />
+      <div className="bn-separator" />
       <h3 className="bn-features-header">Graphics</h3>
-      <p className="bn-subheader"> From left to right: Profile screen, Home screen, Message feed, and Chat screen</p>
-    <div className="bn-images-container">
-      <div className="bn-image-col">
-        <img className="bn-image" src={imagesIndex.BirdNestProfile} alt="Bird Nest profile screen" />
-        <img className="bn-image" src={imagesIndex.BirdNestHome} alt="Bird Nest home screen" />
-      </div>
-      <div className="bn-image-col">
-        <img className="bn-image" src={imagesIndex.BirdNestMessageFeed} alt="Bird Nest message feed screen" />
-        <img className="bn-image" src={imagesIndex.BirdNestChat} alt="Bird Nest chat screen" />
+      <p className="bn-subheader">
+        {" "}
+        From left to right: Profile screen, Home screen, Message feed, and Chat
+        screen
+      </p>
+      <div className="bn-images-container">
+        <div className="bn-image-col">
+          <img
+            className="bn-image"
+            src={imagesIndex.BirdNestProfile}
+            alt="Bird Nest profile screen"
+          />
+          <img
+            className="bn-image"
+            src={imagesIndex.BirdNestHome}
+            alt="Bird Nest home screen"
+          />
+        </div>
+        <div className="bn-image-col">
+          <img
+            className="bn-image"
+            src={imagesIndex.BirdNestMessageFeed}
+            alt="Bird Nest message feed screen"
+          />
+          <img
+            className="bn-image"
+            src={imagesIndex.BirdNestChat}
+            alt="Bird Nest chat screen"
+          />
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default BirdNest;
--- src/sections/TradingBot/TradingBot.tsx
+++ src/sections/TradingBot/TradingBot.tsx
@@ -1,8 +1,109 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { imagesIndex } from "../../assets/images/imagesIndex";
 import "./TradingBot.css";
 
+const birdNestOptions = {
+  root: null,
+  threshold: 0.8,
+};
+
+const birdNestObserverLeft = new IntersectionObserver(function (
+  entries,
+  birdNestObserverLeft
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("slide-in-left");
+      birdNestObserverLeft.unobserve(entry.target);
+    }
+  });
+},
+birdNestOptions);
+
+const birdNestObserverRight = new IntersectionObserver(function (
+  entries,
+  birdNestObserverRight
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("slide-in-right");
+      birdNestObserverRight.unobserve(entry.target);
+    }
+  });
+},
+birdNestOptions);
+
 const TradingBot = () => {
-  return <div>TradingBot</div>;
+  useEffect(() => {
+    const birdNestFeaturesLeft = document.querySelectorAll(".tb-feature-left");
+    birdNestFeaturesLeft.forEach((feature) => {
+      birdNestObserverLeft.observe(feature);
+    });
+
+    const birdNestFeaturesRight =
+      document.querySelectorAll(".tb-feature-right");
+    birdNestFeaturesRight.forEach((feature) => {
+      birdNestObserverRight.observe(feature);
+    });
+
+    return () => {
+      birdNestObserverLeft.disconnect();
+      birdNestObserverRight.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="tb-container">
+      <p className="tb-header">Cryptocurrency Trading Bot</p>
+      <p className="tb-subheader">Tech Stack: Python, Flask, and React</p>
+      <a
+        className="tb-link"
+        href="https://github.com/deondregarcia/crypto-trading-bot"
+        target="_blank"
+      >
+        <h2>Link to GitHub</h2>
+      </a>
+      <div className="tb-separator" />
+      <div className="tb-what">
+        <h3 className="tb-what-header">What is this trading bot?</h3>
+        <p className="tb-header-text">
+          This is a Python crypto trading bot which utilizes the Binance API
+          platform to gather market data and feeds that input into functions
+          that calculate technical indicators. These indicators in turn are fed
+          into algorithms I designed to model popular trading strategies, which
+          output buy, sell, or hold signals.
+        </p>
+      </div>
+      <div className="tb-separator" />
+      <h3 className="tb-features-header">Features</h3>
+      <div className="tb-features">
+        <div className="tb-features-col">
+          <p className="tb-features-text tb-feature-right">
+            <strong>Consumes Binance API</strong> to get real-time market data
+            and generate buy or sell signals based on different sets of
+            indicators
+          </p>
+          <p className="tb-features-text tb-feature-left">
+            Pulls historical data and tests strategies through backtesting
+          </p>
+        </div>
+        <div className="tb-features-col tb-col-right">
+          <p className="tb-features-text tb-feature-right">
+            <strong>Algorithms</strong> which calculate technical indicators and
+            model conventional trading strategies
+          </p>
+          <p className="tb-features-text tb-feature-left">
+            Trading bot interfaced with <strong>Flask server</strong> for remote
+            interaction via <strong>React front-end</strong> client
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TradingBot;
