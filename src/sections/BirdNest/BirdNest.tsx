84dd41156c7a33b5e6aca80266bf531c4a5c66ad
Pinoyburn <deondre.garcia@yahoo.com>
2022-09-21 22:13:26 -0700

    bird nest section finished

--- src/sections/BirdNest/BirdNest.tsx
+++ src/sections/BirdNest/BirdNest.tsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { imagesIndex } from "../../assets/images/imagesIndex";
 import "./BirdNest.css";
 
+const birdNestOptions = {
+  root: null,
+  threshold: 0.8,
+};
+
+const birdNestObserverLeft = new IntersectionObserver(function(entries, birdNestObserver) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("slide-in-left");
+      birdNestObserver.unobserve(entry.target);
+    }
+  })
+}, birdNestOptions);
+
+const birdNestObserverRight = new IntersectionObserver(function(entries, birdNestObserver) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("slide-in-right");
+      birdNestObserver.unobserve(entry.target);
+    }
+  })
+}, birdNestOptions);
+
 const BirdNest = () => {
+
+  useEffect(() => {
+    const birdNestFeaturesLeft = document.querySelectorAll(".bn-feature-left");
+    birdNestFeaturesLeft.forEach(feature => {
+      birdNestObserverLeft.observe(feature);
+    })
+
+    const birdNestFeaturesRight = document.querySelectorAll(".bn-feature-right");
+    birdNestFeaturesRight.forEach(feature => {
+      birdNestObserverRight.observe(feature);
+    })
+  
+    return () => {
+      birdNestObserverLeft.disconnect();
+      birdNestObserverRight.disconnect();
+    }
+  }, [])
+  
+
   return <div className="bn-container">
     <p className="bn-header">Bird Nest</p>
     <p className="bn-subheader">
@@ -20,21 +66,26 @@ const BirdNest = () => {
       <h3 className="bn-features-header">Features</h3>
     <div className="bn-features">
       <div className="bn-features-col">
-        <p className="bn-features-text">Personal profiles with <strong>questionnaires</strong> and displayed traits geared towards housing preferences</p>
-        <p className="bn-features-text"><strong>Sorting algorithms</strong> that rank potential roommates based on number of matched preferences</p>
-        <p className="bn-features-text">Matchmaking system</p>
+        <p className="bn-features-text bn-feature-right">Personal profiles with <strong>questionnaires</strong> and displayed traits geared towards housing preferences</p>
+        <p className="bn-features-text bn-feature-left"><strong>Sorting algorithms</strong> that rank potential roommates based on number of matched preferences</p>
       </div>
-      <div className="bn-features-col">
-      <p className="bn-features-text"><strong>Filtering algorithms</strong> which, on top of the sorting feature, allows users to apply more granular and variable specifications on which type of roommate is desired</p>
-      <p className="bn-features-text"><strong>Messaging</strong> capabilities</p>
-      <p className="bn-features-text">Profile editing, image uploading, and much, much more</p>
+      <div className="bn-features-col bn-col-right">
+      <p className="bn-features-text bn-feature-right"><strong>Filtering algorithms</strong> which, on top of the sorting feature, allows users to apply more granular and variable specifications on which type of roommate is desired</p>
+      <p className="bn-features-text bn-feature-left"><strong>Messaging</strong>, profile editing, <strong>matchmaking</strong>, image uploading, and much, much more</p>
       </div>
     </div>
+    <div className="bn-separator" />
+      <h3 className="bn-features-header">Graphics</h3>
+      <p className="bn-subheader"> From left to right: Profile screen, Home screen, Message feed, and Chat screen</p>
     <div className="bn-images-container">
-      <img src={imagesIndex.BirdNestProfile} alt="Bird Nest profile screen" />
-      <img src={imagesIndex.BirdNestHome} alt="Bird Nest home screen" />
-      <img src={imagesIndex.BirdNestMessageFeed} alt="Bird Nest message feed screen" />
-      <img src={imagesIndex.BirdNestChat} alt="Bird Nest chat screen" />
+      <div className="bn-image-col">
+        <img className="bn-image" src={imagesIndex.BirdNestProfile} alt="Bird Nest profile screen" />
+        <img className="bn-image" src={imagesIndex.BirdNestHome} alt="Bird Nest home screen" />
+      </div>
+      <div className="bn-image-col">
+        <img className="bn-image" src={imagesIndex.BirdNestMessageFeed} alt="Bird Nest message feed screen" />
+        <img className="bn-image" src={imagesIndex.BirdNestChat} alt="Bird Nest chat screen" />
+      </div>
     </div>
   </div>;
 };
--- src/sections/Navbar/Navbar.tsx
+++ src/sections/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div className='navbar-container'>Navbar</div>
+    <div className='navbar-container'></div>
   )
 }
 
